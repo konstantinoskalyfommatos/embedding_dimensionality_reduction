784156bcdc7d68e4ed426fc4fb2002f712fd47cd
Konstantinos <kkalyfo@gmail.com>
2026-01-21 13:40:49 +0200

    make precalculate_embeddings more configurable

--- src/scripts/general/precalculate_embeddings.py
+++ src/scripts/general/precalculate_embeddings.py
@@ -50,6 +50,7 @@ def precalculate_train_embeddings(
     batch_size: int,
     text_column: str,
     dataset_name: str = None,
+    max_examples: int = 2500000
 ):
     """Precalculate embeddings for a dataset using a specified model.
 
@@ -69,48 +70,43 @@ def precalculate_train_embeddings(
 
     model.max_seq_length = 300
 
-    split_to_max_entries = {
-        # "train": 2500000,
-        "validation": 250000
-    }
-    for split, max_entries in split_to_max_entries.items():
-        print(f"Processing split: {split}")
-        ds_split = load_dataset(
-            dataset_path, 
-            dataset_name, 
-            split=split, 
-            streaming=True
-        )
-        print(f"Loaded {split} split from dataset: {dataset_path}")
+    ds_iter = load_dataset(
+        dataset_path, 
+        dataset_name, 
+        split="train", 
+        streaming=True
+    )
 
-        examples = []
-        total_seen = 0
-        # for ex in tqdm(ds_split, desc="Loading examples"):
-        for ex in ds_split:
-            examples.append(ex)
-            total_seen += 1
-            if total_seen >= max_entries:
-                break
+    examples = []
+    total_seen = 0
+    # for ex in tqdm(ds_iter, desc="Loading examples"):
+    for ex in ds_iter:
+        examples.append(ex)
+        total_seen += 1
+        if total_seen >= max_examples:
+            break
 
-        print(f"Filtered {split} dataset to {len(examples)} examples.")
+    print(f"Filtered dataset to {len(examples)} examples.")
 
-        texts = [ex[text_column] for ex in examples]
-        dataset = CustomDataset(texts)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    texts = [ex[text_column] for ex in examples]
+    dataset = CustomDataset(texts)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
-        embeddings = calculate_embeddings(model, dataloader)
+    embeddings = calculate_embeddings(model, dataloader)
 
-        split_output_path = os.path.join(output_base_path, f"{split}_embeddings.pt")
-        os.makedirs(os.path.dirname(split_output_path), exist_ok=True)
-        torch.save(embeddings, split_output_path)
+    split_output_path = os.path.join(output_base_path, f"train_embeddings.pt")
+    os.makedirs(os.path.dirname(split_output_path), exist_ok=True)
+    torch.save(embeddings, split_output_path)
 
-        print(f"Saved {split} embeddings to {split_output_path}")
+    print(f"Saved train embeddings to {split_output_path}")
 
 
 def precalculate_val_test_embeddings(
     model_name: str,
     dataset_path: str,
     batch_size: int,
+    max_validation_examples: int = 10000,
+    max_test_examples: int = 10000
 ):
     """Precalculate embeddings for a dataset using a specified model.
 
@@ -130,16 +126,14 @@ def precalculate_val_test_embeddings(
 
     model.max_seq_length = 512
 
-    split_to_max_entries = {
-        "test": 10000,
-        "validation": 10000
-    }
     ds_iter = load_dataset(
         dataset_path, 
         split="validation",  # Only validation available 
-        # streaming=True
     )
-    for split, max_entries in split_to_max_entries.items():
+    for split, max_examples in zip(
+        ["validation", "test"],
+        [max_validation_examples, max_test_examples]
+    ):
         print(f"Processing split: {split}")
 
         examples = []
@@ -150,7 +144,7 @@ def precalculate_val_test_embeddings(
             if len(text.split()) < 350 and len(paraphrase.split()) < 350: 
                 examples += [text, paraphrase]
                 total_seen += 1
-            if total_seen >= max_entries:
+            if total_seen >= max_examples:
                 break
 
         print(f"Filtered {split} dataset to {len(examples)} examples.")
@@ -170,23 +164,24 @@ def precalculate_val_test_embeddings(
 if __name__ == "__main__":
     parser = ArgumentParser(description="Precalculate embeddings for a dataset")
     parser.add_argument("--model_name", type=str, default="jinaai/jina-embeddings-v2-small-en")
-    parser.add_argument("--dataset_path", type=str, default="allenai/c4", choices=["allenai/c4", "agentlans/sentence-paraphrases"])
-    parser.add_argument("--dataset_name", type=str, default="en")
-    parser.add_argument("--text_column", type=str, default="text")
+    parser.add_argument("--train_or_eval", type=str, choices=["train", "eval"], required=True, help="Precalulate train or validation-test embeddings")
     parser.add_argument("--batch_size", type=int, default=8192)
+    parser.add_argument("--max_train_examples", type=int, default=2500000, help="Maximum number of training examples to process")
+    parser.add_argument("--max_val_examples", type=int, default=10000, help="Maximum number of validation examples to process")
+    parser.add_argument("--max_test_examples", type=int, default=10000, help="Maximum number of test examples to process")
     args = parser.parse_args()
 
-    if args.dataset_path == "allenai/c4":
+    if args.train_or_eval == "train":
         precalculate_train_embeddings(
             model_name=args.model_name,
-            dataset_path=args.dataset_path,
-            dataset_name=args.dataset_name,
+            dataset_path="allenai/c4",
+            dataset_name="en",
             batch_size=args.batch_size,
-            text_column=args.text_column
+            text_column="text"
         )
     else:
         precalculate_val_test_embeddings(
             model_name=args.model_name,
-            dataset_path=args.dataset_path,
+            dataset_path="agentlans/sentence-paraphrases",
             batch_size=args.batch_size
         )
